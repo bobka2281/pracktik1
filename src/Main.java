ed3023f3be5da36db87dcc292cfa32017333d0ff
mister bombino <166628703+bobka2281@users.noreply.github.com>
2025-10-25 16:56:42 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         try {
-            System.out.print("Введите запрос: ");
-            String request = scanner.nextLine();
-
-            Search search = new Search();
-            String resp = search.wiki(request);
-
-            Answer answer = new Answer();
-            Answer.SearchRes result = answer.parse(resp);
-            answer.printRes(result);
-
-            Opener opener = new Opener();
-            opener.selection(scanner, result, answer);
-
+            searchAndShow(scanner);
         } catch (Exception e) {
-            System.out.println("Ошибка: " + e.getMessage());
+            System.out.println(" Произошла Ошибка: " + e.getMessage());
         } finally {
             scanner.close();
         }
+    }
+    private static void searchAndShow(Scanner scanner) throws Exception {
+        System.out.print("Введите запрос: ");
+        String request = scanner.nextLine();
+
+        Search search = new Search();
+        String resp = search.wiki(request);
+
+        Answer answer = new Answer();
+        Answer.SearchRes result = answer.parse(resp);
+
+        if (result.query == null || result.query.search == null || result.query.search.length == 0) {
+            System.out.println("Ничего не найдено. Попробуйте другой запрос.\n");
+            searchAndShow(scanner);
+            return;
+        }
+        answer.printRes(result);
+        Opener opener = new Opener();
+        opener.selection(scanner, result, answer);
     }
 }
